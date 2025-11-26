7374d91830fb6a1bd294f1397ae1f9e980ce5743
Noushad <thedavmetrix@gmail.com>
2025-11-26 22:55:16 +0600

    emni

---
+++ introduction/control_statement/conditionals/conditional.py
@@ -0,0 +1,15 @@
+"""
+if condition:
+    #code block to be executed if condition is true
+else:
+    #code block to be executed if condition is false
+"""
+
+age=int(input("Enter your age : "))
+
+if(age>=18): 
+    print("You are eligible to vote")
+    if 1==1:
+      print("Nested if block")
+else:
+    print("You are not eligible to vote")
