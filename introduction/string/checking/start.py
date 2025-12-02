30304a15a40b227fe92380c6766f4b1176ee56d1
Noushad <thedavmetrix@gmail.com>
2025-12-03 02:17:16 +0600

    full string

---
+++ introduction/string/checking/start.py
@@ -0,0 +1,16 @@
+#startswith used to check whether a string starts with a specific substring or not. It returns True if the string starts with the specified substring, otherwise it returns False.
+str="Python"
+print(str.startswith("P"))
+
+#endswith used to check whether a string ends with a specific substring or not. It returns True if the string ends with the specified substring, otherwise it returns False.
+print(str.endswith("o"))
+
+#isalpha used to check whether all characters in the string are alphabetic (letters) or not. It returns True if all characters are alphabetic, otherwise it returns False.
+
+print(str.isalpha())
+
+#isdigit used to check whether all characters in the string are digits (0-9) or not. It returns True if all characters are digits, otherwise it returns False.
+print(str.isdigit())
+
+#isalnum used to check whether all characters in the string are alphanumeric (letters and digits) or not. It returns True if all characters are alphanumeric, otherwise it returns False.
+print(str.isalnum())
