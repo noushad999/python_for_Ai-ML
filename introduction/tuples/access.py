4acd6f56d868abdcb246ac08d630a063e0674330
Noushad <thedavmetrix@gmail.com>
2025-12-04 02:58:48 +0600

    tuples

---
+++ introduction/tuples/access.py
@@ -0,0 +1,11 @@
+"""
+a=(1,2,3)
+a[0]=100
+print(a) # This will raise a TypeError because tuples are immutable
+
+"""
+
+a=(1,2,3,4,5)
+print(a[0:3:1])
+
+#tuple use korbo shudho jodi data change na korte hoy like month names, days names etc. karon tuple memory te kom jay and fast o kaj kore.
---
+++ introduction/tuples/create.py
@@ -0,0 +1,11 @@
+a=(10,20,30)
+b=()
+print(type(a))
+print(type(b))
+
+"""
+tuple is immutable so ekhane amra list er moto kono element change korte parbo na. or add korte parbo na.
+but amra tuple er moddhe kono element access korte pari index er maddhome. also amra len() function use kore tuple er length o ber korte pari.min max sum o korte pari tuple er moddhe jodi numeric value thake.count o korte pari kono specific element koto bar ache seta.
+index
+sorting o korte pari tuple er moddhe jodi numeric value thake.
+"""
